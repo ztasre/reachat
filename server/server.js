40169851c70014cf9e591546eefb98f8054af340
ztasre <ztasre@devnull.com>
2018-07-24 18:02:18 -0400

    Finished basic logic, need to handle users better and fix the css.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 1337 });
 
 let history = [];
+let users = []
 
 wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
@@ -15,17 +16,15 @@ wss.broadcast = function broadcast(data) {
 }
 
 wss.on('connection', function connections(ws) {
-
+    
+    // initial message sent upon connection to server
+    ws.send(JSON.stringify(history));
+    
     ws.on('message', function incoming(message) {
         console.log(message);
-        history.push(message);
+        history.push(JSON.parse(message));
         wss.broadcast(JSON.stringify(history));
     });
-    
-    /*
-    ws.send(JSON.stringify([{ user: "andrei", message: "hello"}
-                        , { user: "grumpy", message: "quiet!"}]));
-    */
+
 });
 
-//Hello
--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function MessageList(props) {
+function UserList(props) {
 
-    //console.log(props.messages[0])
-    //add {item.user} later
+    let users = new Set();
+
+    props.history.map( (item) => users.add(item.user) );
+
+    const usersList = users.forEach( (user) => 
+        <li key={user.toString()}>
+            {user}
+        </li>
+    );
+
+    return (
+        <div id="user-list">
+        <ul>{usersList}</ul>
+        </div>
+    );
+}
+
+function MessageList(props) {
     
     const elements = props.history.map( (item) =>
             <li key={item.toString()}>
-                {item}
+                {item.user}: {item.message}
             </li>
         );
 
@@ -19,45 +35,94 @@ function MessageList(props) {
     );
 }
 
+class UserSettings extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { username: this.props.user };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        this.setState( { username: event.target.value } );
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.userChange(this.state.username);
+    }
+
+   render() {
+       return (
+           <form onSubmit={this.handleSubmit}>
+           <label>
+           name: 
+           <input type="text"
+                  value={this.state.username}
+                  onChange={this.handleChange} />
+           </label>
+           <input type="submit" value="Change" />
+           </form>
+       )
+   }
+}
+
 class MessageBox extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { message: '' };
-
+        this.state = { user: this.props.user,
+                       message: ''};
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onEnterPress = this.onEnterPress.bind(this);
+
+      //this.handleSubmit = this.handleSubmit.bind(this);
 
     }
 
     handleChange(event) {
-        this.setState({ message: event.target.value });
+        this.setState({ message: event.target.value,
+                        user: this.props.user});
         event.preventDefault();
     }
-
+    
+    /*
     handleSubmit(event) {
-        this.props.socket.send(this.state.message);
+        this.props.socket.send(JSON.stringify(
+            this.state));
+
         event.preventDefault();
 
         this.setState({ message: '' });
     }
+    */
+
+    onEnterPress(event) {
+        if (event.keyCode === 13 && event.shiftKey === false) {
+            event.preventDefault();
+
+            this.props.socket.send(JSON.stringify(
+                    this.state));
+
+            this.setState({ message: '' });
+        }
+    }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input id="message-enter"
+            <form>
+                <textarea id="message-enter"
                        type="text" 
                        value={this.state.message}
-                       onChange={this.handleChange} />
+                       onChange={this.handleChange}
+                       onKeyDown={this.onEnterPress}
+                />
             </form>
         );
     }
 }
-/*
-<input id="message-send"
-                       type="submit"
-                       value="send" />
-                       */
 
 class App extends Component {
     constructor(props) {
@@ -66,6 +131,7 @@ class App extends Component {
         this.state = {
             ws: new WebSocket('ws://localhost:1337'),
             history: [],
+            user: 'default'
         }
         
         // Collection of event handlers for WebSockets.
@@ -90,12 +156,27 @@ class App extends Component {
         this.state.ws.onclose = (e) => {
             console.log('temp close');
         }
+
+        this.handleUserChange = this.handleUserChange.bind(this);
     }
+
+    handleUserChange(username) {
+        this.setState({ user: username});
+    }
+
   render() {
     return (
         <div>
+        <section className="side-pane">
+        <UserList history={this.state.history}/>
+        <UserSettings userChange={this.handleUserChange}
+                      user={this.state.user}/>
+        </section>
+        <section className="center-pane">
         <MessageList history={this.state.history}/>
-        <MessageBox socket={this.state.ws} /> 
+        <MessageBox socket={this.state.ws}
+                    user={this.state.user}/> 
+        </section>
         </div>
     );
   }
